e5490ffbca87668323bf2bc852e3ce014db2c3ed
Anna George <amgeorge30@hotmail.com>
2018-07-25 20:01:31 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,20 +1,20 @@
 
-def create_olympics_hash
-  # Implement this method so that it returns a hash with the data provided on README.md
-  summer_olympics = {"Sydney" => 2000, "Athens" => 2004, "Bejing" => 2008, "London" => 2012}
-  puts summer_olympics
-end
+# def create_olympics_hash
+#   # Implement this method so that it returns a hash with the data provided on README.md
+#   summer_olympics = {"Sydney" => 2000, "Athens" => 2004, "Bejing" => 2008, "London" => 2012}
+#   puts summer_olympics
+# end
 
-create_olympics_hash
-def add_a_key_value_pair
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-   summer_olympics = {"Sydney" => 2000, "Athens" => 2004, "Bejing" => 2008, "London" => 2012}
-   summer_olympics_new = "Atlanta"
-   new_date = 1996
-   summer_olympics[summer_olympics_new] = new_date
-   puts summer_olympics
-end
-add_a_key_value_pair
+# create_olympics_hash
+# def add_a_key_value_pair
+#   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+#   summer_olympics = {"Sydney" => 2000, "Athens" => 2004, "Bejing" => 2008, "London" => 2012}
+#   summer_olympics_new = "Atlanta"
+#   new_date = 1996
+#   summer_olympics[summer_olympics_new] = new_date
+#   puts summer_olympics
+# end
+# add_a_key_value_pair
 
 def iterate_through_hash
           # Implement this method so that it iterates over the hash created in add_a_key_value_pair
